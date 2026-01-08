08789f065bef590404ca6ce3064811f6515b84da
Veena krishnan <veenakrishnan.2525@gmail.com>
2026-01-08 13:50:26 +0530

    Todo app with select, delete, and strike feature

--- js/script.js
+++ js/script.js
@@ -3,40 +3,54 @@ let inputAddTaskBtn = document.getElementById('addTaskBtn');
 let inputTaskList = document.getElementById('taskList');
 let deleteBtn = document.getElementById('deletebtn');
 let selectedtask = null;
-inputAddTaskBtn.addEventListener('click', () =>{
-    let name = inputTaskToComplete .value; 
-    
-    if (name === ""){
+
+// listner for the add btn
+inputAddTaskBtn.addEventListener('click', () => {
+    let name = inputTaskToComplete.value;
+//popup if not enteries are given
+    if (name === "") {
         alert("Please enter task");
         return;
-         
+
     }
     const li = document.createElement('li');
-     li.textContent = `Name : ${name}`;
-     li.className = "list-item";
+    li.textContent = `Name : ${name}`;
+    li.className = "list-item";
 
-     li.addEventListener('click', ()=> {
-        if (selectedtask){
+    //click action that removes the click of previous selected item and the newely item will show selected
+    li.addEventListener('click', () => {
+        if (selectedtask) {
             selectedtask.classList.remove('selected')
         }
         li.classList.add('selected');
         selectedtask = li;
         console.log(selectedtask);
-        
 
-     });
 
-     inputTaskList.appendChild(li);
-     inputTaskToComplete.value = "";
-   
+
+    });
+
+    //toggle added
+
+    li.addEventListener('click', () => {
+        li.classList.toggle('completed');
+        console.log('click');
+
+
+    });
+    inputTaskList.appendChild(li);
+    inputTaskToComplete.value = "";
+
 
 });
-    deleteBtn.addEventListener('click', () =>{
-        if(selectedtask){
-            selectedtask.remove();
-            selectedtask = null;
-            return;
+    // listner for delete btn
 
-        }
-        alert("please select a student");
-     });
+deleteBtn.addEventListener('click', () => {
+    if (selectedtask) {
+        selectedtask.remove();
+        selectedtask = null;
+        return;
+
+    }
+    alert("please select a task");
+});
